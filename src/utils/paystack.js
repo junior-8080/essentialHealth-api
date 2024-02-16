d5921ef9f1298545c8635080c36bc40073166d1a
abdul mukhsin <aabdulmukhsin@yahoo.com>
2024-02-16 20:16:20 +0000

    billing issue

--- src/utils/paystack.js
+++ src/utils/paystack.js
@@ -6,8 +6,8 @@ export const initializeTransaction = async function initializeTransaction(billin
       method: "POST",
       url: "https://api.paystack.co/transaction/initialize",
       data: {
-        ...billingDetails,
-        callback_url: "https://57b8-102-221-28-6.ngrok-free.app/api/v1/billing/payment-webhook"
+        ...billingDetails
+        // callback_url: "https://57b8-102-221-28-6.ngrok-free.app/api/v1/billing/payment-webhook"
       },
       headers: {
         Authorization: `Bearer ${process.env.PAYSTACK_API_KEY_LIVE}`
