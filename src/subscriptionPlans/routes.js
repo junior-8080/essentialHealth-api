534f7b861a01831be1eae6cc6a07924602a58818
abdul mukhsin <aabdulmukhsin@yahoo.com>
2026-06-07 12:48:24 +0000

    fix:subscription plans

--- src/subscriptionPlans/routes.js
+++ src/subscriptionPlans/routes.js
@@ -6,9 +6,9 @@ import {permissions} from "../utils/permissions.js";
 const router = express.Router();
 
 router.post("/", routeAccess(permissions.can_create_subscription_plans), subscriptionPlanControllers.createSubscriptionPlan);
-router.get("/", routeAccess(permissions.can_update_subscription_plans), subscriptionPlanControllers.fetchSubscriptionPlans);
+router.get("/", subscriptionPlanControllers.fetchSubscriptionPlans);
 router.get("/:planId", subscriptionPlanControllers.fetchSubscriptionPlan);
-router.put("/:planId", subscriptionPlanControllers.updateSubscriptionPlan);
+router.put("/:planId", routeAccess(permissions.can_update_subscription_plans), subscriptionPlanControllers.updateSubscriptionPlan);
 router.delete("/:planId", subscriptionPlanControllers.deleteSubscriptionPlan);
 
 export default router;
